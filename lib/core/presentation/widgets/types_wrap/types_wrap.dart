c004fc8222db59b1ac7821d5682e07e99ac5864d
Herman <hdavidmb@gmail.com>
2021-09-03 13:30:07 -0500

    Create TypesWrap widget

---
+++ lib/core/presentation/widgets/types_wrap/types_wrap.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_flutter/core/domain/types.dart';
+
+class TypesWrap extends StatelessWidget {
+  final List<Types> types;
+  final double itemHeight;
+  const TypesWrap({
+    Key? key,
+    required this.types,
+    this.itemHeight = 25.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 4.0,
+      children: types
+          .map((type) => TypeChip(
+                type: type,
+                itemHeight: itemHeight,
+              ))
+          .toList(),
+    );
+  }
+}
+
+class TypeChip extends StatelessWidget {
+  final Types type;
+  final double itemHeight;
+  const TypeChip({
+    Key? key,
+    required this.type,
+    this.itemHeight = 25.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(4.0),
+      height: itemHeight,
+      decoration: BoxDecoration(
+          color: type.chipColor, borderRadius: BorderRadius.circular(4.0)),
+      child: Row(
+        children: [
+          Image(
+            image: type.icon,
+            color: Colors.white,
+          ),
+          const SizedBox(width: 4.0),
+          Text(
+            type.name,
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
