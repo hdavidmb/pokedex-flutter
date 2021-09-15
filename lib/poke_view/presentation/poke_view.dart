e5f9ea928b05559e1c9d0a8103dc55f74ab6bb80
Juan Gonzalo Quiroz Cadavid <juanquiroz@seven4n.com>
2021-09-15 12:02:34 -0500

    Pika loading! There is a fucking bug :'(

--- lib/poke_view/presentation/poke_view.dart
+++ lib/poke_view/presentation/poke_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex_flutter/config/app_images.dart';
 import 'package:pokedex_flutter/core/presentation/app_bar.dart';
+import 'package:pokedex_flutter/core/presentation/widgets/loaders/pika_loading.dart';
+import 'package:pokedex_flutter/poke_view/application/poke_state.dart';
+import 'package:pokedex_flutter/poke_view/application/poke_view_providers.dart';
 import 'package:pokedex_flutter/poke_view/presentation/widgets/poke_header.dart';
 import 'package:pokedex_flutter/poke_view/presentation/widgets/poke_tabs.dart';
 
@@ -8,12 +13,19 @@ class PokeInfoView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final pokeViewProvider = context.read(pokeViewNotifierProvider);
+    pokeViewProvider.fecthPokeData('charm');
+
     return Scaffold(
       appBar: appBar,
       extendBodyBehindAppBar: true,
-      body: Container(
-        child: _Body(),
-      ),
+      body: Container(child: Consumer(builder: (context, watch, child) {
+        final pokeState = watch(pokeViewNotifierProvider).pokeState;
+
+        if (pokeState == PokeState.ready()) return _Body();
+
+        return PikaLoader();
+      })),
     );
   }
 }
